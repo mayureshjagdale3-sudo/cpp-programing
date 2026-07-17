f8329acf4309cbee382d3915fb65a0f74eb4e693
mayureshjagdale3-sudo <mayureshjagdale3@gmail.com>
2026-07-17 07:27:14 +0000

    Added C++ programs

---
+++ add.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    int b;
+    int sum ;
+    cout<<"inter a number : ";
+    cin >> a;
+    cout<<"inter b number :"; 
+    cin >> b;
+    sum=a+b;
+ cout<<"your sum is : " << sum <<endl;
+ return 0;
+}
+ 
+   
